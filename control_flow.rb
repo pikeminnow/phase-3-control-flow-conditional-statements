2b097ba76bf88512af8ea830bbc4ee0207e21942
Ashton MacKenzie <pikeminnow@gmail.com>
2022-10-12 13:15:17 -0400

    commit tree bcd039a7318b7212163c58a33282d0fe3fa310a4

--- control_flow.rb
+++ control_flow.rb
@@ -1,9 +1,30 @@
+require 'pry'
 def admin_login(username, password)
   # your code here
+  if password == '12345'
+    unless username == 'admin'|| username == 'ADMIN'
+      "Access denied"
+    else 
+      "Access granted"
+    end
+  else
+    "Access denied"
+  end
 end
 
 def hows_the_weather(temperature)
   # your code here
+  response =  if temperature < 40
+    "brisk"
+  elsif temperature >= 40 && temperature <= 65
+    "a little chilly"
+  elsif temperature > 85
+    "too dang hot"
+  else
+    "perfect"
+  end
+
+"It's #{response} out there!"
 end
 
 def fizzbuzz(num)
@@ -13,4 +34,3 @@ end
 def calculator(operation, num1, num2)
   # your code here
 end
-
